7d4670a9cfbb5d5c0a78d0748ee235e1bdc4aac9
uperpenta <i.raresandrei@yahoo.com>
2023-08-18 11:11:22 +0300

    sda

---
+++ catBE/src/main/java/com/example/catbe/CatBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.catbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatBeApplication.class, args);
+	}
+
+}
---
+++ catBE/src/test/java/com/example/catbe/CatBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
